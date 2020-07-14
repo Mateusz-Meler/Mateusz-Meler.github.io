e74991025156d2e0992ac2b7a6af0a64ed2729bb
Mateusz Meler <mateuszmeler@icloud.com>
2020-07-14 18:47:38 +0200

    feat: adding github appi for projects section

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,47 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
 
 /* place your code below */
+
+fetch("https://api.github.com/users/mateusz-meler/repos?sort=created")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    for (let repo of resp) {
+      const { name, description, homepage, html_url } = repo;
+      const myGithub = document.querySelector(".github--js");
+      const myTemplate = 
+      `<div class="github">
+  <div class="github__bar">
+      <div class="github__circle"></div>
+      <div class="github__circle"></div>
+      <div class="github__circle"></div>
+    </div>
+    <div class="github__elements">
+    <img class="github__logo" src="public/img/github-logo.svg" alt="github logo">
+    <ul class="list">
+    
+      <li class="list--item">project:</li>
+      <li class="list--item">description:</li>
+      <ul class="secondary-list">
+      <li class="secondary-list--item">demo:</li>
+      <li class="secondary-list--item">github:</li>
+    </ul>
+  </ul>
+    <ul class="date">
+    <li class="date__name"> ${name}</li>
+    <li class="date__description">${description}</li>
+    <li> <a class="date__homepage" href="${homepage}" title="link do demo ${name} na githubie">see here</a></li>
+    <li> <a class="date__github" href="${html_url}" title="link do repozytorium ${name} na githubie">source code</a></li>
+    </ul>
+  </div>
+  </div>`;
+      myGithub.innerHTML += myTemplate;
+    }
+  })
+  .catch((error) => {
+    console.log("Masz gdzieś błąd");
+  });
+
